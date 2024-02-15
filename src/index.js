ead270e8a4f7b05fd8a8a7a3df7d378e78233f62
Mykel68 <115032431+Mykel68@users.noreply.github.com>
2024-02-15 14:16:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const ListenersCount = require('./listenersCountModel'); 
+const ListenersCount = require('./listenersCountModel');
 
 const app = express();
 app.use(express.static('public'));
@@ -16,16 +16,17 @@ mongoose.connect(process.env.MONGO_URL, {
 }).catch((err) => {
     console.log(err.message);
 });
+
 app.get('/api/listenersCount', async (req, res) => {
     try {
-        // Fetch the single document from the database
-        const latestEntry = await ListenersCount.findOne().sort({ _id: -1 }).limit(1);
+        // Fetch the document with the highest count from the database
+        const highestEntry = await ListenersCount.findOne().sort({ count: -1 }).limit(1);
 
-        // Respond with the latest count, event, and live status as JSON
+        // Respond with the highest count, event, and live status as JSON
         res.json({
-            count: latestEntry ? latestEntry.count : 0,
-            event: latestEntry ? latestEntry.event : 'No event',
-            isLive: latestEntry && latestEntry.event.toLowerCase().includes('live'),
+            count: highestEntry ? highestEntry.count : 0,
+            event: highestEntry ? highestEntry.event : 'No event',
+            isLive: highestEntry && highestEntry.event.toLowerCase().includes('live'),
         });
     } catch (error) {
         console.error('Error fetching listeners count:', error.message);
@@ -42,50 +43,68 @@ async function updateListenersCount() {
 
         // Check if the station is live
         if (live.is_live) {
-            // Fetch the single document from the database
-            const latestEntry = await ListenersCount.findOne().sort({ _id: -1 }).limit(1);
+            // Fetch the document with the highest count from the database
+            const highestEntry = await ListenersCount.findOne().sort({ count: -1 }).limit(1);
 
-            // Determine the event based on the day and time
-            let event = '';
-            const currentDay = new Date().getDay();
-            const currentHour = new Date().getHours();
+            // If no document exists or the new count is higher, update the database
+            if (!highestEntry || listeners.current > highestEntry.count) {
+                // Determine the event based on the day and time
+                let event = '';
+                const currentDay = new Date().getDay();
+                const currentHour = new Date().getHours();
 
-            // Update the event based on the day
-            switch (currentDay) {
-                case 0: // Sunday
-                    event = (currentHour >= 7 && currentHour < 12) ? 'Sunday Worship Service' : 'Live Event';
-                    break;
-                case 1: // Monday
-                    event = 'Monday Bible Study';
-                    break;
-                case 2: // Tuesday
-                    event = 'Leaders Development';
-                    break;
-                case 3: // Wednesday
-                    event = 'Live Event';
-                    break;
-                case 4: // Thursday
-                    event = 'Revival Broadcast';
-                    break;
-                case 5: // Friday
-                    event = 'Live Event';
-                    break;
-                case 6: // Saturday
-                    event = 'Workers Training';
-                    break;
-                default:
-                    event = 'Live Event';
-                    break;
-            }
+                 // Check for the special event (GCK)
+                if (currentDay === 4) { // Thursday
+                    const today = new Date();
+                    const lastThursday = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+                    const nextTuesday = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+                    lastThursday.setDate(lastThursday.getDate() - ((lastThursday.getDay() - 4 + 7) % 7));
+                    nextTuesday.setDate(lastThursday.getDate() + 4 + 1);
 
-            // Update the count and event in the database
-            if (latestEntry) {
-                await ListenersCount.findByIdAndUpdate(latestEntry._id, { count: listeners.current, event });
-                console.log('Updated listeners count and event in the database:', listeners.current, event);
-            } else {
-                // If no document exists, create a new one
-                await ListenersCount.create({ count: listeners.current, event });
-                console.log('New document created:', listeners.current, event);
+                    if (today >= lastThursday && today <= nextTuesday) {
+                        event = 'GCK';
+                    }
+                }
+                else {
+
+                    // Update the event based on the day
+                    switch (currentDay) {
+                        case 0: // Sunday
+                            event = (currentHour >= 7 && currentHour < 12) ? 'Sunday Worship Service' : 'Live Event';
+                            break;
+                        case 1: // Monday
+                            event = 'Monday Bible Study';
+                            break;
+                        case 2: // Tuesday
+                            event = 'Leaders Development';
+                            break;
+                        case 3: // Wednesday
+                            event = 'Live Event';
+                            break;
+                        case 4: // Thursday
+                            event = 'Revival Broadcast';
+                            break;
+                        case 5: // Friday
+                            event = 'Live Event';
+                            break;
+                        case 6: // Saturday
+                            event = 'Workers Training';
+                            break;
+                        default:
+                            event = 'Live Event';
+                            break;
+                    }
+                }
+
+                // Update the count and event in the database
+                if (highestEntry) {
+                    await ListenersCount.findByIdAndUpdate(highestEntry._id, { count: listeners.current, event });
+                    console.log('Updated listeners count and event in the database:', listeners.current, event);
+                } else {
+                    // If no document exists, create a new one
+                    await ListenersCount.create({ count: listeners.current, event });
+                    console.log('New document created:', listeners.current, event);
+                }
             }
         } else {
             // If the service is not live, update the event to the day's event and reset the count
